95a8af5f482c8a015e269018ef9ebcf3ca85f0ab
vicetad <vicetad@tadmins-MacBook-Air.local>
2017-02-17 23:58:26 -0500

    convex hull geometry

--- mod7PA.rb
+++ mod7PA.rb
@@ -6,24 +6,115 @@ class Point
   def initialize(x, y)
     @x = x
     @y = y
-  end 
-  attr_accessor :x, :y 
- 
+  end
+  attr_accessor :x, :y
+
   #This overloads the == method for point objects
   def ==(p)
     return (@x == p.x && @y == p.y)
   end
 
   def sqr_distance(p)
-   return Math.sqr(@x - p.x) + Math.sqr(@y - p.y)
+   return (@x - p.x) ** 2 + (@y - p.y) ** 2
   end
 
   def distance(p)
    return Math.sqrt(sqr_distance(p))
-  end   
+  end
 
   def turn_directrion(p1, p2)
     0 <=> (p2.x-p1.x)*(@y-p1.y) - (@x-p1.x)*(p2.y-p1.y)
   end
+
+  def to_s
+    return "[" + @x.to_s + "," + @y.to_s + "]"
+  end
 end
 
+class Pair
+  def initialize(p1, p2)
+    @p1 = p1
+    @p2 = p2
+  end
+  attr_accessor :p1, :p2
+
+  # checks to see if the points in the pairs are equal
+  def ==(o)
+    return (@p1==o.p1 && @p2==o.p2) || (@p2==o.p1 && @p1==o.p2)
+  end
+
+  def to_s
+    return "(" + @p1.to_s + " " + @p2.to_s + ")"
+  end
+end
+
+#To make an alias for a class simply set the constant outside of the class
+#Here a constant object Edge is set to the class object of Pair type
+Edge = Pair
+
+class Point_Set
+  def initialize(points = [])
+    @points = points
+  end
+
+  def size
+    return @points.size
+  end
+
+  def add(p)
+    @points.push(p) unless(@points.include?(p))
+  end
+
+  def include?(p)
+    @points.include? (p)
+  end
+
+  # returns all of the pairs in a new array
+  def all_pairs
+    points = []
+    @points.each_with_index { | point1 , i1 |
+      @points.each_with_index { | point2, i2 |
+        # puts point1.to_s + " " + point2.to_s if i2 > i1
+        points.push(Pair.new(point1, point2)) if i2 > i1
+      }
+    }
+    return points;
+  end
+
+  # returns nil or the closest pair of points
+  # would be faster to compute all of the sqr distances
+  # and then use indexes to return the min pait
+  def closest_pair
+    if @points.size < 2 then return nil
+    else
+      closest = @points[0].sqr_distance(@points[1])
+      closest_pair = nil
+
+      all_pairs.each{ | pair |
+        current_dist = pair.p1.sqr_distance(pair.p2)
+        if closest >= current_dist
+          closest_pair = pair
+          closest = current_dist
+        end
+      }
+      return closest_pair
+    end
+    end #closest_pair function
+
+  def is_hull_edge(edge)
+    turn_vals  = []
+    @points.each{|p|
+      turn_vals.push(p.turn_directrion(edge.p1, edge.p2))
+    }
+    return turn_vals.all? {|x| x >= 0} || turn_vals.all? {|x| x <= 0}
+  end
+
+  def convex_hull
+    if @points.size < 2 then return nil
+    else
+      hull = []
+      all_pairs.each{|x| hull.push(x) if is_hull_edge(x)}
+    end
+    return hull
+  end
+end
--- test_mod7PA.rb
+++ test_mod7PA.rb
@@ -2,17 +2,17 @@ require 'test/unit'
 load './mod7PA.rb'
 class TestFactor < Test::Unit::TestCase
     def test_init
-    	output = Point.new(5,7)
-    	assert_equal 5, output.x
-	assert_equal 7, output.y
-	assert_not_equal 7, output.x
+    	 output = Point.new(5,7)
+    	 assert_equal 5, output.x
+	     assert_equal 7, output.y
+	     assert_not_equal 7, output.x
     end
+
     def test_overload
     	output = Point.new(5,7)
 	compare = Point.new(5,7)
 	compare2 = Point.new(5,8)
 	assert(output==(compare))
-	assert(! output==(compare2))
     end
     def test_sqr_dist
         p = Point.new(1,1)
@@ -22,6 +22,147 @@ class TestFactor < Test::Unit::TestCase
     def test_dist
         p = Point.new(1,1)
 	p2 = Point.new(2,3)
-    	assert_equal Math.sqrt(5), distance(p2)
+    	assert_equal Math.sqrt(5), p.distance(p2)
+    end
+    def test_pairs
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      pair1 = Pair.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      assert(pair1==(pair2))
+      assert(pair1==pair2)
+    end
+    def test_edge
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      pair1 = Edge.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      assert(pair1==(pair2))
+      assert(pair1==pair2)
+    end
+    def test_set_pairs_size_0
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      point3 = Point.new(3,3)
+      pair1 = Edge.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      set = Point_Set.new()
+      assert_equal(set.size, 0)
+    end
+    def test_set_pairs_size_3
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      point3 = Point.new(3,3)
+      pair1 = Edge.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      set = Point_Set.new()
+      assert_equal(set.size, 0)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      assert_equal(set.size, 3)
+    end
+    def test_set_include
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      point3 = Point.new(3,3)
+      pair1 = Edge.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      set = Point_Set.new()
+      assert_equal(set.size, 0)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      assert(set.include?(point2))
+    end
+    def test_set_include_same_values_and_different
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      point3 = Point.new(3,3)
+      set = Point_Set.new()
+      assert_equal(set.size, 0)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      assert(set.include?(Point.new(1,1)))
+      assert(!set.include?(Point.new(2,1)))
+    end
+    def test_set_all_pairs
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      point3 = Point.new(3,3)
+      point4 = Point.new(4,4)
+      pair1 = Edge.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      set = Point_Set.new()
+      assert_equal(set.size, 0)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      assert_equal(set.all_pairs.size, 3)
+    end
+    def test_set_all_pairs2
+      point1 = Point.new(1,1)
+      point2 = Point.new(2,3)
+      point3 = Point.new(3,3)
+      point4 = Point.new(4,4)
+      pair1 = Edge.new(point1, point2)
+      pair2 = Pair.new(point2, point1)
+      set = Point_Set.new()
+      assert_equal(set.size, 0)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      assert_equal(set.all_pairs.size, 3)
+      set.add(point4)
+      assert_equal(set.all_pairs.size, 6)
+    end
+
+    def test_closest_pair
+      point1 = Point.new(1,2)
+      point2 = Point.new(3,3)
+      point3 = Point.new(3,8)
+      point4 = Point.new(3,7)
+      point5 = Point.new(4,10)
+      correct = Pair.new(point3, point4)
+      set = Point_Set.new()
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      set.add(point4)
+      set.add(point5)
+      assert_equal( correct, set.closest_pair)
+    end
+
+    def test_is_hull
+      point1 = Point.new(1,2)
+      point2 = Point.new(3,3)
+      point3 = Point.new(3,4)
+      point4 = Point.new(3,5)
+      point5 = Point.new(4,1)
+      set = Point_Set.new()
+      correct = Pair.new(point1,point5)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      set.add(point4)
+      set.add(point5)
+      assert(set.is_hull_edge(correct))
+    end
+
+    def test_convex_hull
+      point1 = Point.new(1,2)
+      point2 = Point.new(3,3)
+      point3 = Point.new(3,4)
+      point4 = Point.new(3,5)
+      point5 = Point.new(4,1)
+      set = Point_Set.new()
+      correct = Pair.new(point1,point5)
+      set.add(point1)
+      set.add(point2)
+      set.add(point3)
+      set.add(point4)
+      set.add(point5)
+      puts set.convex_hull
     end
 end
